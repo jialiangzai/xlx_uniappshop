d87866f5e087929c1eea7b76bfd132dfabf77efb
zuishuainanhai <1705902650@qq.com>
2022-06-28 01:15:13 +0800

    电商项目基本架子

---
+++ xlx_shopping/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="index">
+<!-- <view class="fColor">
+  Hello World
+</view>
+<view class="icon-message">
+  
+</view>
+<view class="icon-search"></view> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
