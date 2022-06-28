28b8a52c2194c873c5c550aa7a387764d5669cf0
zuishuainanhai <1705902650@qq.com>
2022-06-29 00:08:38 +0800

    顶部开发

--- xlx_shopping/pages/index/index.vue
+++ xlx_shopping/pages/index/index.vue
@@ -1,29 +1,25 @@
 <template>
-	<view class="index">
-<!-- <view class="fColor">
+  <view class="index">
+    <!-- <view class="fColor">
   Hello World
-</view>
-<view class="icon-message">
-  
-</view>
-<view class="icon-search"></view> -->
-	</view>
+</view> -->
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+  export default {
+    data() {
+      return {
+        title: 'Hello'
+      }
+    },
+    onLoad() {
 
-		},
-		methods: {
+    },
+    methods: {
 
-		}
-	}
+    }
+  }
 </script>
 
 <style>
