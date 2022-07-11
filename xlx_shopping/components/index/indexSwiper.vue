857ca203a8da52eeedb0d96b1d88ba8879d9bb7e
‘jhx’ <123@qq.com>
2022-07-11 16:42:31 +0800

    swiper部分

---
+++ xlx_shopping/components/index/indexSwiper.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="swiperBox">
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item>
+				<view class="swiper-item" >
+					<img class="swiper-img" src="../../static/img/swiper00.jpg" alt="">
+				</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item" >
+					<img class="swiper-img" src="../../static/img/swiper01.jpg" alt="">
+				</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item" >
+					<img class="swiper-img" src="../../static/img/swiper01.jpg" alt="">
+				</view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"index",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped >''
+/* 在非小程序的情况下是固定高度 */
+swiper{
+	width: 100%;
+	height: 400rpx;
+}
+.swiper-img{
+	width: 100%;
+	height: 400rpx;
+}
+</style>
--- xlx_shopping/pages/index/index.vue
+++ xlx_shopping/pages/index/index.vue
@@ -1,27 +1,46 @@
 <template>
-  <view class="index">
-    <!-- <view class="fColor">
+	<view class="index">
+		<!-- <view class="fColor">
   Hello World
 </view> -->
-  </view>
+		<!-- #ifdef MP-WEIXIN -->
+		<view class="wx-nav">
+			<view class="icon-search"></view>
+			<text class="textTitle">开心元素</text>
+			<view class="icon-message"></view>
+		</view>
+		<!-- #endif -->
+		<indexSwiper></indexSwiper>
+	</view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        title: 'Hello'
-      }
-    },
-    onLoad() {
-
-    },
-    methods: {
-
-    }
-  }
+import indexSwiper from '@/components/index/indexSwiper';
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	components: {
+		indexSwiper
+	},
+	onLoad() {},
+	methods: {}
+};
 </script>
 
 <style>
-
+.wx-nav {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 200rpx;
+	line-height: 200rpx;
+	text-align: center;
+}
+.textTitle {
+	margin: 0 20px;
+}
 </style>
