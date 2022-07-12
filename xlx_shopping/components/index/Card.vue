1e3109b2bc0a514bbd07e09084b76a793c20d9c9
‘jhx’ <123@qq.com>
2022-07-12 09:23:59 +0800

    推荐模块

---
+++ xlx_shopping/components/index/Card.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="Card">
+		<view>
+			
+		</view>
+		<slot>
+			
+		</slot>
+		<view class="card-name">
+			猜你喜欢
+		</view>
+		<view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			cardTitle: {
+				type: String,
+				default: ''
+			},
+		},
+		data() {
+			return {
+				
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.Card{
+		padding: 20rpx 0;
+		display: flex;
+		justify-content: center;
+		font-weight: bolder;
+	}
+	.card-name{
+		padding: 0 20rpx;
+		font-size: 16px;
+	}
+</style>
---
+++ xlx_shopping/components/index/Recommed.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="recommed bg">
+		<view class="recommed-item">
+			<!-- 大图 -->
+			<img class="bigBox" src="../../static/img/body.jpg" alt="">
+			<!-- 小图 -->
+			<view class="smallBox">
+				<img class="item-img" src="../../static/img/maoyi.jpg" alt="">
+				<img class="item-img" src="../../static/img/sahngyi.jpg" alt="">
+				<img class="item-img" src="../../static/img/qunzi.jpg" alt="">
+			</view>
+		</view>
+		
+		<view class="recommed-item">
+			<!-- 大图 -->
+			<img class="bigBox" src="../../static/img/run.jpg" alt="">
+			<!-- 小图 -->
+			<view class="smallBox">
+				<img class="item-img" src="../../static/img/beizi.jpg" alt="">
+				<img class="item-img" src="../../static/img/zhentou.jpg" alt="">
+				<img class="item-img" src="../../static/img/xiezi.jpg" alt="">
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+	.recommed{
+		padding: 20rpx;
+	}
+	.recommed-item{
+		display: flex;
+		flex-direction: column;
+		border-radius: 20rpx;
+		border:  2rpx solid #ccc;
+		overflow: hidden;
+		margin: 20rpx 0;
+	}
+	.bigBox{
+		width: 100%;
+		height: 300rpx;
+	}
+	.smallBox{
+		width: 100%;
+		height: 240rpx;
+	}
+	.item-img{
+		width: 33.333333%;
+		height: 240rpx;
+	}
+</style>
