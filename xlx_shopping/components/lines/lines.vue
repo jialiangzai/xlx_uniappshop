b4f459da45061e544001047b6b87ee6e85b4b3cc
‘jhx’ <123@qq.com>
2022-07-16 17:55:00 +0800

    内容布局

---
+++ xlx_shopping/components/lines/lines.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="Linebox">
+
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "lines",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style>
+	.Linebox {
+		width: 100%;
+		height: 2rpx;
+		background-color: #e1e1e1;
+	}
+</style>
--- xlx_shopping/pages/search/search.vue
+++ xlx_shopping/pages/search/search.vue
@@ -1,18 +1,37 @@
 <template>
-	<view>
-		
+	<view class="search">
+		<lines></lines>
+		<view class="search-item">
+			<!-- 历史记录 -->
+			<view class="search-title">
+				<view>
+					历史记录
+				</view>
+				<view class="icon-search">
+
+				</view>
+			</view>
+			<!-- 热门搜索 -->
+			<view class="">
+
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import lines from '@/components/lines/lines.vue'
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
+		components: {
+			lines
+		},
 		methods: {
-			
+
 		}
 	}
 </script>
